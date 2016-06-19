e2f8f4eb7d701a6377216b42c854710536390389
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-06-19 12:47:04 -0400

    add the new data end point on the server and polling on the client

--- server.rb
+++ server.rb
@@ -1,8 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 
+
+get '/' do
+  erb(:index)
+end
 
 get '/api/comments' do
-  erb(:index)
+  data = {id: 1, author: "Pete dadada", text: "This is one comment"},
+  {id: 2, author: "Jordan lalala", text: "This is *another* comment"}
+
+  return data.to_json
+
 end
